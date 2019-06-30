1778adc5b06bc7569a2562982b4cea2e48a9e6ca
Nasib <abu.nasib@northsouth.edu>
2019-07-01 00:29:32 +0600

    fetch data from registration form and check username already exist or not #9

--- Project-Code/includes/registration.inc.php
+++ Project-Code/includes/registration.inc.php
@@ -1,1 +1,27 @@
 
+<?php
+
+if(isset($_POST['submit'])){
+    include_once 'dbh.inc.php';
+    $name = mysqli_real_escape_string($conn,$_POST['full_name']);
+    $uid = mysqli_real_escape_string($conn,$_POST['uid']);
+    $email = mysqli_real_escape_string($conn,$_POST['email']);
+    $pass = mysqli_real_escape_string($conn,$_POST['pass']);
+    $phone = mysqli_real_escape_string($conn,$_POST['phone']);
+
+    $sql = "SELECT * FROM users WHERE user_uid = '$uid'";
+    $result=mysqli_query($conn,$sql);
+    $resultCheck=mysqli_num_rows($result);
+    if($resultCheck>0){
+        echo "UserNmae already exist";
+    }
+    else{
+
+    }
+
+
+
+} else{
+    header("Location: ../registration.php");
+    exit();
+}
