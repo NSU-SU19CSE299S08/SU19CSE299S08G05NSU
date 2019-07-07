d44c87973c4be7b283d55a1896da55f34e00a55a
Nayeem M. Muzahid <nayeem.muzahid@northsouth.edu>
2019-07-08 01:54:01 +0600

    issue #14: email field validating

--- Project-Code/js/form-validation.js
+++ Project-Code/js/form-validation.js
@@ -1,14 +1,17 @@
 function regValidation() {
-  var uemail = document.getElementById('uemail').value;
+  var userEmail = document.getElementById('uemail').value;
 
   var emailRegx = /^([a-zA-Z\.\-_0-9]+)@([a-zA-Z]+).([a-zA-Z\.]{2,})$/;
 
-  if (emailRegx.test(uemail)) {
+  if (emailRegx.test(userEmail)) {
     document.getElementById('email__label1').style.visibility = 'visible';
     document.getElementById('email__label1').innerHTML = 'valid';
+    return false;
     console.log('was submitted');
   } else {
     document.getElementById('email__label1').style.visibility = 'visible';
     document.getElementById('email__label1').innerHTML = 'invalid';
+    return false;
+    console.log('was not submitted');
   }
 }
