cf9ef0a52f0f2b758de49b5e4bbf4d2240256acf
Nayeem M. Muzahid <nayeem.muzahid@northsouth.edu>
2019-07-08 02:58:08 +0600

    issue #14: validating user Name

--- Project-Code/js/form-validation.js
+++ Project-Code/js/form-validation.js
@@ -1,8 +1,10 @@
 function regValidation() {
-  var userEmail = document.getElementById('uemail').value;
   var uFullName = document.getElementById('fullName').value;
+  var uUserName = document.getElementById('uname').value;
+  var userEmail = document.getElementById('uemail').value;
 
   var fullNameRegx = /^[a-zA-Z \.]{3,}$/;
+  var userNameRegx = /^[a-z0-9_]{4,}$/;
   var emailRegx = /^([a-zA-Z\.\-_0-9]+)@([a-zA-Z]+).([a-zA-Z\.]{2,})$/;
   // full name validation
 
@@ -16,7 +18,19 @@ function regValidation() {
     document.getElementById('fullName__label1').innerHTML =
       'Name can contain characters, space and dot(.)';
   }
+  // username validation
+  if (fullNameRegx.test(uFullName)) {
+    document.getElementById('fullName__label1').style.visibility = 'hidden';
+    document.getElementById('fullName').style.border = '1px solid green';
+  } else {
+    document.getElementById('fullName__label1').style.visibility = 'visible';
+    document.getElementById('fullName__label1').style.color = 'red';
+    document.getElementById('fullName').style.border = '1px solid red';
+    document.getElementById('fullName__label1').innerHTML =
+      'Name can contain characters, space and dot(.)';
+  }
 
+  // email validation
   if (emailRegx.test(userEmail)) {
     document.getElementById('email__label1').style.visibility = 'hidden';
     document.getElementById('email__label1').style.color = 'green';
