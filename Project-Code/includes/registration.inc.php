5be77f315602508d8f1d89d1a5bea1668efbecbc
Sparrow48 <abu.nasib@northsouth.edu>
2019-07-07 12:52:22 +0600

    Issue #9: added comments to the code.

--- Project-Code/includes/registration.inc.php
+++ Project-Code/includes/registration.inc.php
@@ -2,14 +2,18 @@
 
 if(isset($_POST['submit'])){
 
-    include_once 'dbh.inc.php';
+    // connection between client and server
+    include_once 'dbh.inc.php';  
+
+    // data fetch from from client 
 
     $name = mysqli_real_escape_string($conn,$_POST['full_name']);
     $uid = mysqli_real_escape_string($conn,$_POST['uid']);
     $email = mysqli_real_escape_string($conn,$_POST['email']);
     $phone = mysqli_real_escape_string($conn,$_POST['phone']);
     $pass = mysqli_real_escape_string($conn,$_POST['pass']);
-    
+
+    // Check user already exist or not.
 
     $sql = "SELECT * FROM users WHERE user_uid = '$uid'";
     $result=mysqli_query($conn,$sql);
@@ -18,29 +22,39 @@ if(isset($_POST['submit'])){
         echo "UserNmae already exist";
     }
     else{
-        if(empty($name) || empty($uid) || empty($email) ||empty($pass)|| empty($phone)) {
+        //check for empty fields
+
+        if(empty($name) || empty($uid) || empty($email) ||empty($pass)|| empty($phone)) {   
 
             header("Location: ../registration.php?signup=empty");
             exit();
         }else{
-            if (!preg_match("/^[a-zA-Z]*$/", $name)) {
+            // check the valid name
+
+            if (!preg_match("/^[a-zA-Z]*$/", $name)) {                                    
                 header("Location: ../registration.php?signup=invalid_Name");
                 exit();
             }else{
-                if(!filter_var($email, FILTER_VALIDATE_EMAIL)){
+                // email validation
+
+                if(!filter_var($email, FILTER_VALIDATE_EMAIL)){                           
                     header("Location: ../registration.php?signup=invalid_email");
                     exit();
                 }
                 else{
-                    if(!is_numeric($phone)){
+                    // phone number validation 
+
+                    if(!is_numeric($phone)){                                              
                         header("Location: ../registration.php?signup=invalid_phoneNo");
                         exit(); 
                     }
                     else{
-                        $Sql = "INSERT INTO users(name, user_id, user_email, user_phone,user_pass)
+                        // send data into the server
+
+                        $Sql = "INSERT INTO users(full_name, user_uid, user_email, user_phone,user_pass)
                         VALUES('$name','$uid','$email','$phone','$pass')";
                         mysqli_query($conn,$sql);
-                        header("Location: ../success.php?signup=success");
+                        header("Location: ../registration.php?signup=success");
                          exit();
                     }
                     
