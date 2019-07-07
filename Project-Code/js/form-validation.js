4e379f2cd15ed167322c57e43bb5d8e0c4bad8d3
Nayeem M. Muzahid <nayeem.muzahid@northsouth.edu>
2019-07-08 01:20:17 +0600

    issue #14: validation registration form

--- Project-Code/js/form-validation.js
+++ Project-Code/js/form-validation.js
@@ -1,7 +1,7 @@
-function loginValidation() {
+function regValidation() {
   var uemail = document.getElementById('uemail').value;
 
-  var regx = /^([a-zA-Z0-9\.-]+)@([a-zA-Z0-9-]+)\.([a-z]{2-8})(\.[a-z]{2-8})?$/;
+  var regx = /^([a-zA-Z\.\-_0-9]+)@([a-zA-Z]+).([a-zA-Z\.]{2,})$/;
 
   if (regx.test(uemail)) {
     document.getElementById('email__label1').style.visibility = 'visible';
