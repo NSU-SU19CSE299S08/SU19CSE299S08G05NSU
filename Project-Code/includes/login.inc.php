18739969e019e23f6d41937d676c6c0935d9316e
Sparrow48 <abu.nasib@northsouth.edu>
2019-07-08 01:04:34 +0600

    Issue #18: write code for database connection and fetch data from login form.

---
+++ Project-Code/includes/login.inc.php
@@ -0,0 +1,15 @@
+
+<?php
+
+session_start();
+
+if(isset($_POST['submit'])){
+    // after click the login button page will connect to tha database
+
+    include 'dbh,inc.php';
+
+    // fetch data from client
+
+    $uid = mysqli_real_escape_string($conn,$_POST['uid']);
+    $uid = mysqli_real_escape_string($conn,$_POST['pass']);
+}
