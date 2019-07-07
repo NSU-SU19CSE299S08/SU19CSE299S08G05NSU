2e53fd2f820aac13fa414fb48ac2aa08efea7105
Nayeem M. Muzahid <nayeem.muzahid@northsouth.edu>
2019-07-08 02:52:57 +0600

    issue #14: full name validated

--- Project-Code/js/form-validation.js
+++ Project-Code/js/form-validation.js
@@ -7,19 +7,27 @@ function regValidation() {
   // full name validation
 
   if (fullNameRegx.test(uFullName)) {
+    document.getElementById('fullName__label1').style.visibility = 'hidden';
+    document.getElementById('fullName').style.border = '1px solid green';
+  } else {
+    document.getElementById('fullName__label1').style.visibility = 'visible';
+    document.getElementById('fullName__label1').style.color = 'red';
+    document.getElementById('fullName').style.border = '1px solid red';
+    document.getElementById('fullName__label1').innerHTML =
+      'Name can contain characters, space and dot(.)';
   }
 
   if (emailRegx.test(userEmail)) {
     document.getElementById('email__label1').style.visibility = 'hidden';
     document.getElementById('email__label1').style.color = 'green';
-    document.getElementById('uemail').style.border = '1.5px solid green';
+    document.getElementById('uemail').style.border = '1px solid green';
     document.getElementById('email__label1').innerHTML = 'valid';
     return false;
     console.log('was submitted');
   } else {
     document.getElementById('email__label1').style.visibility = 'visible';
     document.getElementById('email__label1').style.color = 'red';
-    document.getElementById('uemail').style.border = '1.5px solid red';
+    document.getElementById('uemail').style.border = '1px solid red';
     document.getElementById('email__label1').innerHTML = 'invalid';
     return false;
     console.log('was not submitted');
