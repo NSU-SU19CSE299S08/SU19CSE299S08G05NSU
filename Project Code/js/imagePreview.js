6e05b2d0bede34548a0fe267ccd030b5631896b9
Nayeem M. Muzahid <nayeem.muzahid@northsouth.edu>
2019-07-13 20:33:28 +0600

    Feature #27: completed the form creation and validation

--- Project Code/js/imagePreview.js
+++ Project Code/js/imagePreview.js
@@ -3,7 +3,7 @@ function readURL(input) {
     var reader = new FileReader();
 
     reader.onload = function(e) {
-      $('#blah').attr('src', e.target.result);
+      $('#img').attr('src', e.target.result);
     };
 
     reader.readAsDataURL(input.files[0]);
