bc053d0fd656ef0f1f3ff6f0aab31a34b8280170
Sparrow48 <abu.nasib@northsouth.edu>
2019-07-14 10:00:40 +0600

    Feature #18 : if-else condition to check empty field and user exist or not
    First if-else condition will check the form empty or not. And later if-else condition will check whether user exist or not.

--- Project Code/includes/login.inc.php
+++ Project Code/includes/login.inc.php
@@ -11,6 +11,23 @@ if(isset($_POST['submit'])){
 
     $uid = mysqli_real_escape_string($conn,$_POST['uid']);
     $uid = mysqli_real_escape_string($conn,$_POST['pass']);
+
+    if(empty($uid)|| empty($pass)){
+        header("Location:../login.php?login=error");
+        exit();
+    }else{
+
+        $sql="SELECT * FROM user WHERE user_uid='$uid'";
+        $result=mysqli_query($conn,$sql);
+        $resultCheck=mysqli_num_rows($result);
+
+        if($resultCheck <1){
+            header("Location:../login.php?login=error");
+			exit();
+        }else{
+            
+        }
+    }
 }
 else{
     header("Location:../login.php?login=error");
