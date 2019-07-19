365cabe027476957165c0b56bac8cbc616311e78
Nayeem M. Muzahid <nayeem.muzahid@northsouth.edu>
2019-07-20 01:22:13 +0600

    Feature #37: adding logic to insert data into table

---
+++ Project Code/includes/bookInfo.php
@@ -0,0 +1,9 @@
+<?php
+
+
+include_once 'dbh.inc.php'; 
+
+$bookName = $_POST['bookName'];
+$authorName = $_POST['authorName'];
+$publisherName = $_POST['publisherName'];
+$isbnNumber = $_POST['isbnNumber'];
