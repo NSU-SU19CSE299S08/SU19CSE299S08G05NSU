d75196fc23f1c44317ba88c52f39f8ceb50f8ef4
Nayeem M. Muzahid <nayeem.muzahid@northsouth.edu>
2019-07-20 01:33:29 +0600

    Feature #37: adding logic to insert data into table

--- Project Code/includes/bookInfo.php
+++ Project Code/includes/bookInfo.php
@@ -1,11 +1,13 @@
 <?php
+if (isset($_POST['submit']))
 
+    include_once 'dbh.inc.php';
 
-include_once 'dbh.inc.php';
+$bookName = mysqli_real_escape_string($conn, $_POST['bookName']);
+$authorName = mysqli_real_escape_string($conn, $_POST['authorName']);
+$publisherName = mysqli_real_escape_string($conn, $_POST['publisherName']);
+$isbnNumber = mysqli_real_escape_string($conn, $_POST['isbnNumber']);
+$bookPrice = mysqli_real_escape_string($conn, $_POST['bookPrice']);
+$bookLanguage = mysqli_real_escape_string($conn, $_POST['bookLanguage']);
 
-$bookName = $_POST['bookName'];
-$authorName = $_POST['authorName'];
-$publisherName = $_POST['publisherName'];
-$isbnNumber = $_POST['isbnNumber'];
-$bookPrice = $_POST['bookPrice'];
-$bookLanguage = $_POST['bookLanguage'];
+$sql = "INSERT INTO bookinfo (BookName,AuthorName,PublisherName,ISBN,BookPrice,BookLanguage) VALUES ('$bookName','$authorName','$publisherName','$isbnNumber','$bookPrice','$bookLanguage')";
