15ed61018f1148ad29720f1357c3a1bcaab7128a
Nayeem M. Muzahid <nayeem.muzahid@northsouth.edu>
2019-07-20 01:23:46 +0600

    Feature #37: fetching data from input fields

--- Project Code/includes/bookInfo.php
+++ Project Code/includes/bookInfo.php
@@ -1,9 +1,11 @@
 <?php
 
 
-include_once 'dbh.inc.php'; 
+include_once 'dbh.inc.php';
 
 $bookName = $_POST['bookName'];
 $authorName = $_POST['authorName'];
 $publisherName = $_POST['publisherName'];
 $isbnNumber = $_POST['isbnNumber'];
+$bookPrice = $_POST['bookPrice'];
+$bookLanguage = $_POST['bookLanguage'];
