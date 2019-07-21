84cec0f1502070b836d08a345c28cae7e346d7d9
Sparrow48 <abu.nasib@northsouth.edu>
2019-07-21 20:53:37 +0600

    Feature #11 : check  the book is exist or not.

--- Project Code/includes/booksearch.inc.php
+++ Project Code/includes/booksearch.inc.php
@@ -9,7 +9,16 @@ if (isset($_POST['submit'])) {
         header("Location:../index1.php?login=error");
         exit();
     }else{
-        
+        $sql="SELECT * FROM bookinfo WHERE BookName='$search'";
+        $result=mysqli_query($conn,$sql);
+        $resultCheck=mysqli_num_rows($result);
+
+        if($resultCheck<1){
+            header("Location:../index1.php?login=error");
+            exit();
+        }else{
+            
+        }
 
     }
 
