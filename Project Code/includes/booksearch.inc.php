b30050e80e07c0795e971f3a1e35331520b623c4
Sparrow48 <abu.nasib@northsouth.edu>
2019-07-21 20:45:55 +0600

    Feature #11 : fetch data from client and check is it empty or not

--- Project Code/includes/booksearch.inc.php
+++ Project Code/includes/booksearch.inc.php
@@ -3,6 +3,16 @@ if (isset($_POST['submit'])) {
 
     include_once 'dbh.inc.php';
 
+    $search = mysqli_real_escape_string($conn,$_POST['search']);
+
+    if(empty($search)){
+        header("Location:../index1.php?login=error");
+        exit();
+    }else{
+        
+
+    }
+
     
 } else {
     echo 'not submitted';
