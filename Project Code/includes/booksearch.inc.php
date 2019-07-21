d5ad2f42c1287665b0a3877bba526a754dbb60a8
Sparrow48 <abu.nasib@northsouth.edu>
2019-07-22 00:23:55 +0600

    Feature #11 : add some comments.

--- Project Code/includes/booksearch.inc.php
+++ Project Code/includes/booksearch.inc.php
@@ -1,14 +1,19 @@
 <?php
 if (isset($_POST['submit'])) {
 
+    //connect to the database
     include_once 'dbh.inc.php';
 
+    //fatch data from client
     $search = mysqli_real_escape_string($conn,$_POST['search']);
 
+    //check for empty string
     if(empty($search)){
         header("Location:../index1.php?login=error");
         exit();
     }else{
+
+        //fatch data from database
         $sql="SELECT * FROM bookinfo WHERE BookName='$search'";
         $result=mysqli_query($conn,$sql);
         $resultCheck=mysqli_num_rows($result);
