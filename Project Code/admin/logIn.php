4ab22cf2c8c493e08450e0b4c4715a490d89a556
Sparrow48 <abu.nasib@northsouth.edu>
2019-07-28 00:42:30 +0600

    Feature #44 : Fetch data and check is it empty or not.

--- Project Code/admin/logIn.php
+++ Project Code/admin/logIn.php
@@ -6,4 +6,15 @@ if(isset($_POST['submit'])){
     // after click the login button page will connect to tha database
 
     include 'dbh.inc.php';
+
+    // fetch data from client
+
+    $aid = mysqli_real_escape_string($conn,$_POST['aid']);
+    $apass = mysqli_real_escape_string($conn,$_POST['apass']);
+
+    // check tht aid and password is empty or not.
+    if(empty($aid) || empty($apass)){
+        header("Location:../login.php?login=error");
+        exit();
+    }
 }
