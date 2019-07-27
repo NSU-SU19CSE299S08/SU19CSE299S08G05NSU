6e59bd6c6e7b79fc53e8790ac1be7859b77bd383
Sparrow48 <abu.nasib@northsouth.edu>
2019-07-28 00:56:50 +0600

    Feature #44 : if match the pass, active all data's session.

--- Project Code/admin/logIn.php
+++ Project Code/admin/logIn.php
@@ -10,10 +10,10 @@ if(isset($_POST['submit'])){
     // fetch data from client
 
     $aid = mysqli_real_escape_string($conn,$_POST['aid']);
-    $apass = mysqli_real_escape_string($conn,$_POST['apass']);
+    $pass = mysqli_real_escape_string($conn,$_POST['apass']);
 
     // check tht aid and password is empty or not.
-    if(empty($aid) || empty($apass)){
+    if(empty($aid) || empty($pass)){
         header("Location:../login.php?login=error");
         exit();
     }else{
@@ -28,6 +28,28 @@ if(isset($_POST['submit'])){
             header("Location:../login.php?login=error");
 			exit();
         }else{
+            if($row=mysqli_fetch_assoc($result)){
+
+                // check whether pass is wrong or not.
+                if($pass != $row['admin_pass']){
+                    header("Location:../login.php?login=wrong_password");
+					exit();
+                }else{
+                    $_SESSION['a_id']=$row['admin_id'];
+                    $_SESSION['name']=$row['full_name'];
+                    $_SESSION['a_uid']=$row['admin_uid'];
+                    $_SESSION['a_email']=$row['admin_email'];
+                    $_SESSION['a_phone']=$row['admin_phone'];
+
+                    //need to add the page path which will apear after successful login.
+                    header("Location:../#.php?login=success") 
+                }
+            }
+        }
 
     }
+}else{
+     // go back to the login page.
+     header("Location:../login.php?login=error");
+     exit();
 }
