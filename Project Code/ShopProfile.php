97c088e97551e408d9ab30a2bf14c4c0001a8470
Nayeem M. Muzahid <nayeem.muzahid@northsouth.edu>
2019-07-28 18:30:03 +0600

    Feature #40: connected database to fetch data

---
+++ Project Code/ShopProfile.php
@@ -0,0 +1,199 @@
+<?php
+include_once 'includes/dbh.inc.php'
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <meta http-equiv="X-UA-Compatible" content="ie=edge" />
+  <title>Profile || Shop</title>
+
+  <!-- -css files- -->
+  <link rel="stylesheet" href="css/all.min.css" />
+  <link rel="stylesheet" href="css/bootstrap.min.css" />
+  <link rel="stylesheet" href="css/owl.carousel.css" />
+  <link rel="stylesheet" href="css/owl.theme.default.min.css" />
+  <link rel="stylesheet" href="css/ShopProfile.css" />
+  <link rel="stylesheet" href="css/index1.css" />
+
+  <!-- js files -->
+
+  <script src="js/jquery.js"></script>
+  <script src="js/bootstrap.min.js"></script>
+  <script src="js/popper.min.js"></script>
+  <script src="js/owl.carousel.min.js"></script>
+</head>
+
+<body>
+  <div class="shopInfo">
+    <div class="shopProfile">
+      <div class="container">
+        <div class="row my-4 shopHeader">
+          <div class="col-md-6 shopProfile-1 text-center">
+            <div class="profileInfo">
+              <img class="my-5" src="img/book6.jpg" alt="" />
+              <h2 class="text-light mb-5">Sheba Publishers</h2>
+            </div>
+          </div>
+          <div class="col-md-6 shopProfile-2  text-light">
+            <h4 class=" mt-5 mb-4 text-center">Owner: Md. Abu Nasib</h4>
+            <div class="profileInfo-2 text-center">
+              <p class="lead">Email: something@example.com</p>
+              <p class="lead">Phone Number: 0195555555</p>
+              <p class="lead">Shop Address: Uttara Sector #13</p>
+              <p class="lead">Employee: 10</p>
+              <p class="lead">Number of Books: 300</p>
+            </div>
+            <button class="btn btn-danger text-right  mt-3 mb-4 ">
+              Edit
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+  <div class="shopBooks">
+    <div class="container">
+      <div class="row text-center">
+        <div class="col-md-3">
+          <div class="bf19book1">
+            <div class="bf19-content">
+              <a href="" title="ভাইরে আপুরে!!! (বই, লেখাপড়া, জীবন)">
+                <img src="img/bookfair19/vaireapure.jpg" alt="ভাইরে আপুরে!!! (বই, লেখাপড়া, জীবন)" /></a>
+              <a href="" title="ভাইরে আপুরে!!! (বই, লেখাপড়া, জীবন)">
+                <h5>ভাইরে আপুরে!!!</h5>
+              </a>
+              <a href="">
+                <p class="lead">শাব্বির আহসান</p>
+              </a>
+            </div>
+          </div>
+        </div>
+        <div class="col-md-3">
+          <div class="bf19book1">
+            <div class="bf19-content">
+              <a href="" title="ভাইরে আপুরে!!! (বই, লেখাপড়া, জীবন)">
+                <img src="img/bookfair19/vaireapure.jpg" alt="ভাইরে আপুরে!!! (বই, লেখাপড়া, জীবন)" /></a>
+              <a href="" title="ভাইরে আপুরে!!! (বই, লেখাপড়া, জীবন)">
+                <h5>ভাইরে আপুরে!!!</h5>
+              </a>
+              <a href="">
+                <p class="lead">শাব্বির আহসান</p>
+              </a>
+            </div>
+            <!-- <button class="btn btn-danger" type="button" name="button">
+                      Add to List
+                    </button> -->
+          </div>
+        </div>
+        <div class="col-md-3">
+          <div class="bf19book1">
+            <div class="bf19-content">
+              <a href="" title="ভাইরে আপুরে!!! (বই, লেখাপড়া, জীবন)">
+                <img src="img/bookfair19/vaireapure.jpg" alt="ভাইরে আপুরে!!! (বই, লেখাপড়া, জীবন)" /></a>
+              <a href="" title="ভাইরে আপুরে!!! (বই, লেখাপড়া, জীবন)">
+                <h5>ভাইরে আপুরে!!!</h5>
+              </a>
+              <a href="">
+                <p class="lead">শাব্বির আহসান</p>
+              </a>
+            </div>
+            <!-- <button class="btn btn-danger" type="button" name="button">
+                      Add to List
+                    </button> -->
+          </div>
+        </div>
+        <div class="col-md-3">
+          <div class="bf19book1">
+            <div class="bf19-content">
+              <a href="" title="ভাইরে আপুরে!!! (বই, লেখাপড়া, জীবন)">
+                <img src="img/bookfair19/vaireapure.jpg" alt="ভাইরে আপুরে!!! (বই, লেখাপড়া, জীবন)" /></a>
+              <a href="" title="ভাইরে আপুরে!!! (বই, লেখাপড়া, জীবন)">
+                <h5>ভাইরে আপুরে!!!</h5>
+              </a>
+              <a href="">
+                <p class="lead">শাব্বির আহসান</p>
+              </a>
+            </div>
+            <!-- <button class="btn btn-danger" type="button" name="button">
+                      Add to List
+                    </button> -->
+          </div>
+        </div>
+        <div class="col-md-3">
+          <div class="bf19book1">
+            <div class="bf19-content">
+              <a href="" title="ভাইরে আপুরে!!! (বই, লেখাপড়া, জীবন)">
+                <img src="img/bookfair19/vaireapure.jpg" alt="ভাইরে আপুরে!!! (বই, লেখাপড়া, জীবন)" /></a>
+              <a href="" title="ভাইরে আপুরে!!! (বই, লেখাপড়া, জীবন)">
+                <h5>ভাইরে আপুরে!!!</h5>
+              </a>
+              <a href="">
+                <p class="lead">শাব্বির আহসান</p>
+              </a>
+            </div>
+            <!-- <button class="btn btn-danger" type="button" name="button">
+                      Add to List
+                    </button> -->
+          </div>
+        </div>
+        <div class="col-md-3">
+          <div class="bf19book1">
+            <div class="bf19-content">
+              <a href="" title="ভাইরে আপুরে!!! (বই, লেখাপড়া, জীবন)">
+                <img src="img/bookfair19/vaireapure.jpg" alt="ভাইরে আপুরে!!! (বই, লেখাপড়া, জীবন)" /></a>
+              <a href="" title="ভাইরে আপুরে!!! (বই, লেখাপড়া, জীবন)">
+                <h5>ভাইরে আপুরে!!!</h5>
+              </a>
+              <a href="">
+                <p class="lead">শাব্বির আহসান</p>
+              </a>
+            </div>
+            <!-- <button class="btn btn-danger" type="button" name="button">
+                      Add to List
+                    </button> -->
+          </div>
+        </div>
+        <div class="col-md-3">
+          <div class="bf19book1">
+            <div class="bf19-content">
+              <a href="" title="ভাইরে আপুরে!!! (বই, লেখাপড়া, জীবন)">
+                <img src="img/bookfair19/vaireapure.jpg" alt="ভাইরে আপুরে!!! (বই, লেখাপড়া, জীবন)" /></a>
+              <a href="" title="ভাইরে আপুরে!!! (বই, লেখাপড়া, জীবন)">
+                <h5>ভাইরে আপুরে!!!</h5>
+              </a>
+              <a href="">
+                <p class="lead">শাব্বির আহসান</p>
+              </a>
+            </div>
+            <!-- <button class="btn btn-danger" type="button" name="button">
+                      Add to List
+                    </button> -->
+          </div>
+        </div>
+        <div class="col-md-3">
+          <div class="bf19book1">
+            <div class="bf19-content">
+              <a href="" title="ভাইরে আপুরে!!! (বই, লেখাপড়া, জীবন)">
+                <img src="img/bookfair19/vaireapure.jpg" alt="ভাইরে আপুরে!!! (বই, লেখাপড়া, জীবন)" /></a>
+              <a href="" title="ভাইরে আপুরে!!! (বই, লেখাপড়া, জীবন)">
+                <h5>ভাইরে আপুরে!!!</h5>
+              </a>
+              <a href="">
+                <p class="lead">শাব্বির আহসান</p>
+              </a>
+            </div>
+            <!-- <button class="btn btn-danger" type="button" name="button">
+                      Add to List
+                    </button> -->
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</body>
+
+</html>
