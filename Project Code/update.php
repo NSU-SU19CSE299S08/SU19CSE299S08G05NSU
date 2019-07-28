14c4d474cf8733eff7cb59e04c1af963494fb52d
Sparrow48 <abu.nasib@northsouth.edu>
2019-07-29 01:15:12 +0600

    Feature #46 : Frontend  html code for update.php

--- Project Code/update.php
+++ Project Code/update.php
@@ -16,3 +16,38 @@ if(!isset($_SESSION['a_id']))
 include_once "dbh.inc.php";
 
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+
+    <meta http-equiv="X-UA-Compatible" content="ie=edge" />
+    <title>Login</title>
+
+    <!-- css files -->
+    <link rel="stylesheet" href="css/all.min.css" />
+    <link rel="stylesheet" href="css/bootstrap.min.css" />
+    <link rel="stylesheet" href="css/login.css" />
+    <link
+      href="https://fonts.googleapis.com/css?family=Open+Sans"
+      rel="stylesheet"
+    />
+
+    <!-- js files- -->
+
+    <script src="js/jquery.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+    <script src="js/popper.min.js"></script>
+  </head>
+  <body>
+    <div class="main">
+      <div class="loginForm">
+        <h1 class="text-center text-dark">Update</h1>
+        <form action="admin/update.inc.php" method=" POST" onsubmit="return regValidation();">
+    </div>
+
+    <script src="js/form-validation.js"></script>
+  </body>
+</html>
