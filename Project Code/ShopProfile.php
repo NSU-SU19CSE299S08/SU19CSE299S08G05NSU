3d33a911976821767d561f87d4d7c426b37261dd
Nayeem M. Muzahid <nayeem.muzahid@northsouth.edu>
2019-08-10 21:25:45 +0600

    Feature #40: Fetching data for book details

--- Project Code/ShopProfile.php
+++ Project Code/ShopProfile.php
@@ -74,8 +74,8 @@ include_once 'includes/dbh.inc.php'
           <div class="col-md-3">
             <div class="bf19book1">
               <div class="bf19-content">
-                <a href="" title="<?php echo $row['BookName'];  ?>">
-                  <img src="" alt="<?php echo $row['BookName'];  ?>" /></a>
+                <a href="index1.php?page=bookDetails&ID=<?php echo $row['ID']?>" title="<?php echo $row['BookName'];  ?>">
+                  <img src="img/bookfair19/vaireapure.jpg" alt="<?php echo $row['BookName'];  ?>" /></a>
                 <a href="" title="<?php echo $row['BookName'];  ?>">
                   <h5><?php echo $row['BookName']; ?></h5>
                 </a>
