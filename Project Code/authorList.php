8aeb1de2214397d50f46409ed8e081af6170960c
Nayeem M. Muzahid <nayeem.muzahid@northsouth.edu>
2019-08-11 02:10:26 +0600

    Feature #57: created layout for author list

---
+++ Project Code/authorList.php
@@ -0,0 +1,46 @@
+<?php  ?>
+
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Authors || BookFinder</title>
+
+    <!-- -css files- -->
+  <link rel="stylesheet" href="css/all.min.css" />
+  <link rel="stylesheet" href="css/bootstrap.min.css" />
+  <link rel="stylesheet" href="css/owl.carousel.css" />
+  <link rel="stylesheet" href="css/owl.theme.default.min.css" />
+   <link rel="stylesheet" href="css/authorList.css">
+  <link rel="stylesheet" href="css/index1.css" />
+
+  <!-- js files -->
+
+  <script src="js/jquery.js"></script>
+  <script src="js/bootstrap.min.js"></script>
+  <script src="js/popper.min.js"></script>
+  <script src="js/owl.carousel.min.js"></script>
+</head>
+<body>
+    <header>
+        
+    </header>
+    <section class="author">
+        <div class="container">
+            <div class="row text-center">
+                <div class="col-md-3 authorProfile">
+                    <a href="">
+                        <img src="img/author/author.jpg" alt="">
+                        <h3 class="mt-3">name goes here</h3>
+                    </a>
+                </div>
+                
+            </div>
+        </div>
+    </section>
+</body>
+</html>
