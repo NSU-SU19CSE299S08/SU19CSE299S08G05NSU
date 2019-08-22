070ea4f3d83f63d01b8df4dcf5ad134a51e45bfe
Sparrow48 <abu.nasib@northsouth.edu>
2019-08-22 22:13:47 +0600

    Feature #46 : add code for view data.

--- Project Code/update.php
+++ Project Code/update.php
@@ -1,17 +1,20 @@
 <?php
 session_start();
 
-?>
-
-<?php
-session_start();
-
 if(!isset($_SESSION['a_id']))
 {
 	header("Location:login.php");
     exit;
 }
 include_once "dbh.inc.php";
+
+?>
+
+
+<?php
+include_once "dbh.inc.php";
+$query = "SELECT * FROM users";
+$result = mysqli_query($conn,$query);
 
 ?>
 
@@ -25,9 +28,9 @@ include_once "dbh.inc.php";
     <title>Update</title>
 
     <!-- css files -->
-    <link rel="stylesheet" href="css/all.min.css" />
-    <link rel="stylesheet" href="css/bootstrap.min.css" />
-    <link rel="stylesheet" href="css/login.css" />
+    <link rel="stylesheet" href="../css/all.min.css" />
+    <link rel="stylesheet" href="../css/bootstrap.min.css" />
+    <link rel="stylesheet" href="../css/login.css" />
     <link
       href="https://fonts.googleapis.com/css?family=Open+Sans"
       rel="stylesheet"
@@ -35,12 +38,12 @@ include_once "dbh.inc.php";
 
     <!-- js files- -->
 
-    <script src="js/jquery.js"></script>
-    <script src="js/bootstrap.min.js"></script>
-    <script src="js/popper.min.js"></script>
+    <script src="../js/jquery.js"></script>
+    <script src="../js/bootstrap.min.js"></script>
+    <script src="../js/popper.min.js"></script>
   </head>
   <body>
-    <nav class="navbar navbar-dark bg-dark  navbar-expand-md">
+        <nav class="navbar navbar-dark bg-dark  navbar-expand-md">
 
                 <div class="container">
                   <a class="navbar-brand active" href="#">Home</a>
@@ -53,28 +56,46 @@ include_once "dbh.inc.php";
                   </ul>
               </div>
             </nav>
+            <h1 class="display-4">User List:</h1>
 
-    <div class="main">
-      <div class="loginForm">
-        <h1 class="text-center text-dark">Update</h1>
-        <form action="admin/update.inc.php" method=" POST" onsubmit="return regValidation();">
-    </div>
-    <script src="js/form-validation.js"></script>
+         <div class="table-info">
+              <table class="table table-bordered table-hover">
+                <thead class="table-light ">
+                  <tr>
+                    <th>ID</th>
+                    <th>First_Name</th>
+                   <th>User_Name</th>
+                   <th>Email</th>
+                  <th>password</th>
+                  </tr>
+                </thead>
+                <tbody>
 
+     <?php
+    while($sql = mysqli_fetch_assoc($result)) {?>
+    <tr>
+      <td><?php echo $sql['uid'] ?></td>
+      <td><?php echo $sql['f_name'] ?></td>
+      <td><?php echo $sql['u_name'] ?></td>
+      <td><?php echo $sql['u_email'] ?></td>
+      <td><?php echo $sql['u_email'] ?></td>
+      <td><?php echo $sql['u_pass'] ?></td>
+
+    </tr>
     <?php
-         include 'includes/dbh.inc.php';
-         $sql="SELECT * FROM users ";
-		 $result=mysqli_query($conn,$sql);
 
-    ?>
+     };?>
 
-    <table>
-         	<tr>
-                 <th>User_name</th>
-                 <th>v_uid</th>
-         		<th>v_email</th>
-         		<th>v_phone</th>
-             </tr>
-    </table>
+</tbody>
+
+  </table>
+</div>
+
+</div>
+
+
+</div>
+    <script src="../js/form-validation.js"></script>
+
   </body>
 </html>
