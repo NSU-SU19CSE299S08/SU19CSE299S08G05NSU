cb412968616c876d9c1ba5e4325884b05c2931cb
Sparrow48 <abu.nasib@northsouth.edu>
2019-08-23 11:49:00 +0600

    Fearute #62 : Front-end code for deleteUser page.

--- Project Code/deleteUser.php
+++ Project Code/deleteUser.php
@@ -5,3 +5,35 @@ $query = "SELECT * FROM users";
 $result = mysqli_query($conn,$query);
 
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+
+    <meta http-equiv="X-UA-Compatible" content="ie=edge" />
+    <title>Update</title>
+
+    <!-- css files -->
+    <link rel="stylesheet" href="css/all.min.css" />
+    <link rel="stylesheet" href="css/bootstrap.min.css" />
+    <link rel="stylesheet" href="css/login.css" />
+    <link
+      href="https://fonts.googleapis.com/css?family=Open+Sans"
+      rel="stylesheet"
+    />
+
+    <!-- js files- -->
+
+    <script src="js/jquery.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+    <script src="js/popper.min.js"></script>
+  </head>
+  <body>
+      
+
+    <script src="../js/form-validation.js"></script>
+
+  </body>
+</html>
