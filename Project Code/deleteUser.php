8a026cdb9d29deb1c0276f96f31cd9fae8e96ecd
Sparrow48 <abu.nasib@northsouth.edu>
2019-08-23 12:13:33 +0600

    Feature #62 : Code for showing data.

--- Project Code/deleteUser.php
+++ Project Code/deleteUser.php
@@ -9,29 +9,29 @@ $result = mysqli_query($conn,$query);
 <!DOCTYPE html>
 <html lang="en">
   <head>
-    <meta charset="UTF-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <meta charset="UTF-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
 
-    <meta http-equiv="X-UA-Compatible" content="ie=edge" />
-    <title>Update</title>
+        <meta http-equiv="X-UA-Compatible" content="ie=edge" />
+        <title>Update</title>
 
-    <!-- css files -->
-    <link rel="stylesheet" href="css/all.min.css" />
-    <link rel="stylesheet" href="css/bootstrap.min.css" />
-    <link rel="stylesheet" href="css/login.css" />
-    <link
-      href="https://fonts.googleapis.com/css?family=Open+Sans"
-      rel="stylesheet"
-    />
+        <!-- css files -->
+        <link rel="stylesheet" href="css/all.min.css" />
+        <link rel="stylesheet" href="css/bootstrap.min.css" />
+        <link rel="stylesheet" href="css/login.css" />
+        <link
+         href="https://fonts.googleapis.com/css?family=Open+Sans"
+        rel="stylesheet"
+        />
 
-    <!-- js files- -->
+        <!-- js files- -->
 
-    <script src="js/jquery.js"></script>
-    <script src="js/bootstrap.min.js"></script>
-    <script src="js/popper.min.js"></script>
-  </head>
+        <script src="js/jquery.js"></script>
+        <script src="js/bootstrap.min.js"></script>
+        <script src="js/popper.min.js"></script>
+    </head>
   <body>
-  <nav class="navbar navbar-dark bg-dark  navbar-expand-md">
+             <nav class="navbar navbar-dark bg-dark  navbar-expand-md">
 
                 <div class="container">
                   <a class="navbar-brand active" href="index.php">Home</a>
@@ -56,8 +56,29 @@ $result = mysqli_query($conn,$query);
                   </tr>
                 </thead>
                 <?php
-      
 
+                while ($row=mysqli_fetch_array($result)) {
+
+                    echo "<tr><form action='includes/delete.inc.php' method=POST>";
+                    echo "<td><input type=text name=id value='".$row['user_id']."'</td>";
+                    echo "<td><input type=text name=Name value='".$row['full_name']."'</td>";
+                    echo "<td><input type=text name=uid value='".$row['user_uid']."'</td>";
+                    echo "<td><input type=text name=email value='".$row['user_email']."'</td>";
+                    echo "<td><input type=text name=phone value='".$row['user_phone']."'</td>";
+
+                    echo "<td><input type=submit value=Delete>";
+                    echo"</form></tr>";
+                }
+
+
+                  ?>
+        </table>
+        </div>
+
+        </div>
+
+
+        </div>
     <script src="../js/form-validation.js"></script>
 
   </body>
