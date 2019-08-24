da264f5512e6ff210b0e49b72bdea551c9539257
Sparrow48 <abu.nasib@northsouth.edu>
2019-08-24 18:15:23 +0600

    Feature #44 : Modify database connection.

--- Project Code/includes/login.inc.php
+++ Project Code/includes/login.inc.php
@@ -1,11 +1,9 @@
 <?php
 
-session_start();
-
 if(isset($_POST['submit'])){
     // after click the login button page will connect to tha database
 
-    include 'dbh.inc.php';
+    include_once 'dbh.inc.php';
 
     // fetch data from client
 
@@ -56,3 +54,4 @@ else{
 exit();
 }
 
+?>
