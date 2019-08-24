7050cfe0a9550d932ee9e45c1e3d981e8274d2eb
Sparrow48 <abu.nasib@northsouth.edu>
2019-08-25 00:36:12 +0600

    Feature #75 : add CSS and Js code for front-end

--- Project Code/Ahome.php
+++ Project Code/Ahome.php
@@ -3,3 +3,33 @@ session_start();
 include_once 'includes/dbh.inc.php';
 
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <meta name="author" content="Nayeem, Nasib" />
+
+  <meta http-equiv="X-UA-Compatible" content="ie=edge" />
+  <title>Welcome to BoiMela Online Service</title>
+  <!-- -css files- -->
+  <link rel="stylesheet" href="css/all.min.css" />
+  <link rel="stylesheet" href="css/bootstrap.min.css" />
+  <link rel="stylesheet" href="css/owl.carousel.css" />
+  <link rel="stylesheet" href="css/owl.theme.default.min.css" />
+  <link rel="stylesheet" href="css/index1.css" />
+
+  <!-- js files -->
+
+  <script src="js/jquery.js"></script>
+  <script src="js/bootstrap.min.js"></script>
+  <script src="js/popper.min.js"></script>
+  <script src="js/owl.carousel.min.js"></script>
+
+  <script src="js/custom.js"></script>
+  <script src="js/index1.js"></script>
+</head>
+
+<body>
